dc1d45e5d444e146c26a01f19fe3c58216f7411f
steve <steve.berenger@gmail.com>
2024-08-16 13:06:15 +0200

    gitignore

---
+++ projet_web_atrio/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using projet_web_atrio.Models;
+
+namespace projet_web_atrio.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Personne> Personnes { get; set; }
+
+        public DbSet<Emploi> Emplois { get; set; }
+
+    }
+}
---
+++ projet_web_atrio/Pages/Privacy.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using projet_web_atrio.Data;
+using projet_web_atrio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projet_web_atrio.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        DataContext _dataContext;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger, DataContext dataContext)
+        {
+            _logger = logger;
+            this._dataContext = dataContext;
+        }
+
+        public void OnGet()
+        {
+            var personne = new Personne() { Nom = "Toto", Prenom = "Tata", DateDeNaissance = new DateTime(1990, 5, 15) };
+            _dataContext.Personnes.Add(personne);
+            _dataContext.SaveChanges();
+
+        }
+    }
+}
